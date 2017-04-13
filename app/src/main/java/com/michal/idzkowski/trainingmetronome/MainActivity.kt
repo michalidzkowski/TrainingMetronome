1c71174a62a5d3cc2617b578abcbe84ea9302543
Michal Idzkowski <michal.idzkowski@gmail.com>
2017-04-13 21:00:46 +0200

    Initial commit

---
+++ app/src/main/java/com/michal/idzkowski/trainingmetronome/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.michal.idzkowski.trainingmetronome
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
