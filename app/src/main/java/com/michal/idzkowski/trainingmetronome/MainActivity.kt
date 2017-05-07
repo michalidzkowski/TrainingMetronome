3f8039a2ec58ffd78f73c028df82b0db25914c85
Michal Idzkowski <michal.idzkowski@gmail.com>
2017-05-07 11:25:52 +0200

    Add fragment support for main activity

--- app/src/main/java/com/michal/idzkowski/trainingmetronome/MainActivity.kt
+++ app/src/main/java/com/michal/idzkowski/trainingmetronome/MainActivity.kt
@@ -2,11 +2,31 @@ package com.michal.idzkowski.trainingmetronome
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onBackPressed() {
+        val count = supportFragmentManager.backStackEntryCount
+        if (count == 0) {
+            super.onBackPressed()
+        } else {
+            supportFragmentManager.popBackStack()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        super.onSupportNavigateUp()
+        onBackPressed()
+        return true
     }
 }
